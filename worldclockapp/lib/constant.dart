f46fe3a1e41e794f62911290f321c31e0cf7a209
handekarai <handekarai.hk@gmail.com>
2022-06-21 21:46:41 +0300

    constants added; packages added

---
+++ worldclockapp/lib/constant.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const darkColor =  Color(0xFF002359);
+const dark2Color =  Color(0xFF02327D);
+
+const whiteColor = Color(0xFFFFFFFF);
+const lightBg = Color(0xFFFAFCFF);
+
+
+const lightBlue = Color(0xFFE3EEFF);
+const strokeBlue = Color(0xFF8FAFE0);
